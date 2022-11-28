c7f950a58b06ebc557d62be4e219ca3e0e3e4a1e
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-28 16:20:51 +0100

    script snack1

---
+++ snack1/js/script.js
@@ -0,0 +1,19 @@
+
+
+/*
+
+    Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+
+*/
+
+let i = 0;
+
+
+while(i != 5){
+
+    i = parseInt(prompt("inserire un numero tra 1 e 10"));
+    
+}
+
+console.log(i);
