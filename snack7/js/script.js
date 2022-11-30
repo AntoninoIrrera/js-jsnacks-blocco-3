a967c435fa1c5c61b9f483edcfefe8cf8e832520
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-30 11:30:13 +0100

    snack7

---
+++ snack7/js/script.js
@@ -0,0 +1,48 @@
+/**
+* Crea due tag div con due id diversi.
+* Un div avrà il testo colorato di rosso mentre l'altro di verde.
+* Partendo da un array di numeri,
+* creiamo un nuovo elemento p e lo stampiamo nell'div rosso  se è un numero dispari
+* e nel div verde se è un numero pari.
+*
+*
+*/
+// funzione che crei un nuovo elemento <p> </p>
+// restituirlo a chi invoca la funzione
+//
+// ciclo per ogni elemento dell'array
+// se l'elemento è un numero pari, crei nuovo elemento e lo aggiungo al div verde
+// se l'elemento è un numero dispari, creo  nuovo elemento e lo aggiungo al div rosso
+
+
+
+
+const array = [1, 33, 55, 64, 33, 22, 10, 44];
+
+
+const dispari = document.getElementById("outputDispari");
+const pari = document.getElementById("outputPari");
+
+
+function createElement(){
+
+    pElement = document.createElement("p");
+    return pElement;
+}
+
+
+
+for(let i = 0; i < array.length; i++){
+    
+    let p = createElement();
+    
+    if(array[i] % 2 == 1){
+        dispari.append(p);
+        p.append(array[i]);
+    }else{
+        pari.append(p);
+        p.append([array[i]])
+    }
+
+
+}
